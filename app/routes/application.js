511db258125b503eb26e23dc3e9b14238dc1b0e9
Nicholas Char <nick.char.nick@gmail.com>
2018-09-28 15:13:59 -0400

    return json

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,8 +4,7 @@ import fetch from 'fetch';
 const DATA_HOST = 'https://gist.githubusercontent.com/allthesignals/f9224f5b2860e19c900180f295f0dbe7/raw/ad81c60619ee95932dd6957a22a1a8d8b0e6a3b8';
 export default Route.extend({
   model() {
-    return fetch(
-      `${DATA_HOST}/tutorial_chicago_bike_routes.geojson`
-    );
+    return fetch(`${DATA_HOST}/tutorial_chicago_bike_routes.geojson`)
+      .then(response => response.json());
   }
 });
