553fa3f68ea705e184d1a4a19da56c42732a650f
Nicholas Char <nick.char.nick@gmail.com>
2018-09-28 15:11:24 -0400

    fetch bike lane data

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import fetch from 'fetch';
 
+const DATA_HOST = 'https://gist.githubusercontent.com/allthesignals/f9224f5b2860e19c900180f295f0dbe7/raw/ad81c60619ee95932dd6957a22a1a8d8b0e6a3b8';
 export default Route.extend({
   model() {
-    return {};
+    return fetch(
+      `${DATA_HOST}/tutorial_chicago_bike_routes.geojson`
+    );
   }
 });
